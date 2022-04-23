2f895ab59d4d5d73db26c2a8b72b319435cbabc6
Felix Etchegaray <flixromer443@gmail.com>
2022-04-23 20:18:33 -0300

    todas las facturas instantaneas modificadas

--- js/validatePayForm.js
+++ js/validatePayForm.js
@@ -1,25 +1,41 @@
 let i=0
 let codes=[]
 let prices=[]
+let dates=[]
+
 function addToPayment(code,element){
 
     if(element.checked){
         let price=document.getElementById('price-'+code).value
-        if(price>0){
-            codes.push(code)
-            prices.push(price)
-        }else{
-            alert("Ingrese un monto por favor.")
+        let date=document.getElementById('date-'+code).value
+        
+        let res=validateForm(date,price)
+        if(res===1){
+            alert("todos los campos son obligatorios")
             element.checked=false
         }
-        i++;
+        if(res===2){
+            document.getElementById('date-'+code).value=""
+            alert("ingrese una fecha valida")
+            element.checked=false
+            
+        }else if(res===0){
+            codes.push(code)
+            prices.push(price)
+            dates.push(date)
+            i++;
+        }
+        
     }else{
         document.getElementById('price-'+code).value=""
+        document.getElementById('date-'+code).value=""
+
         index=codes.indexOf(code)
         codes.splice(index,1)
         index=prices.indexOf(code)
         prices.splice(index,1)
-
+        index=dates.indexOf(code)
+        dates.splice(index,1)
         i--;
 
     }
@@ -30,11 +46,39 @@ function pay(action){
     let number=$('#number').val()
     switch(action){
         case 1:
-            window.open(`./components/factura_instantanea.php?pid=${number}&&partner=${partner}&&codes=${codes}&&prices=${prices} `)
+            window.open(`./components/factura_instantanea.php?pid=${number}&&partner=${partner}&&codes=${codes}&&prices=${prices}&&dates=${dates} `)
         break;
         default:
-            window.open(`./components/factura_instantanea_afip.php?pid=${number}&&partner=${partner}&&codes=${codes}&&prices=${prices} `)
+            window.open(`./components/factura_instantanea_afip.php?pid=${number}&&partner=${partner}&&codes=${codes}&&prices=${prices}&&dates=${dates} `)
         break;
             
     }
+}
+
+function autoCompleteDate(element){
+    let value=element.value
+    if(value.length===2 || value.length===5){
+        value+="/"
+        document.getElementById(element.id).value=value
+    }
+    
+}
+
+
+function validateForm(date,price){
+    let dateSeparated=date.split("/")
+    //1
+    if(date.length===10 && price.length>0){
+        //2
+        if(parseInt(dateSeparated[0])>=1 && parseInt(dateSeparated[0])<=31){
+            //2
+            if(parseInt(dateSeparated[1])>=1 && parseInt(dateSeparated[1])<=12){
+                //codes.push(code)
+                //prices.push(price)
+                //dates.push(date)
+                return 0
+            }else{return 2}
+        }else{return 2}   
+    }else{return 1}
+    
 }
